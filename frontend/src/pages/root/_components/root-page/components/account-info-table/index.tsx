3e144427b2ffaf6944e6d61922e080dea34f3f43
roman_zh1 <roman_zh1@vk.com>
2024-05-14 00:31:50 +0300

    fix: account table

--- frontend/src/pages/root/_components/root-page/components/account-info-table/index.tsx
+++ frontend/src/pages/root/_components/root-page/components/account-info-table/index.tsx
@@ -10,61 +10,64 @@ import { useValidRouteParams } from 'src/libs';
 import styles from './index.module.scss';
 
 export const UserInfoTable = memo(() => {
-  const { id } = useValidRouteParams('root');
-  const { data } = queries.guardian.useGetAllAccountsById({ id });
-  const [hiddenFields, setHiddenFields] = useState<{ [key: string]: boolean }>({});
+    const { id } = useValidRouteParams('root');
+    const { data } = queries.guardian.useGetAllAccountsById({ id });
+    const [hiddenFields, setHiddenFields] = useState<{ [key: string]: boolean }>({});
 
-  const toggleVisibility = (field: string) => {
-    setHiddenFields(prev => ({ ...prev, [field]: !prev[field] }));
-  };
+    const toggleVisibility = (field: string) => {
+        setHiddenFields(prev => ({ ...prev, [field]: !prev[field] }));
+    };
 
-  if (data)
-    return (
-        <Box className={styles.root}>
-          <Paper elevation={3} className={styles.block}>
-            {[
-              { title: 'Main Information', fields: [
-                  { label: 'Name', value: data.name, secret: false },
-                  { label: 'User name', value: data.user_name, secret: false },
-                  { label: 'Email', value: data.email, secret: false },
-                  { label: 'Password', value: data.password, secret: true }
-                ]},
-              { title: 'Websites', fields: data.websites?.map(website => ({ label: 'Website', value: website, secret: false })) || []},
-              { title: 'Custom Fields', fields: data.custom_fields?.map(field => ({ label: field.key, value: field.value, secret: false })) || [] }
-            ].map((section, index) => (
-                <div key={index}>
-                  <Typography variant="h4" className={styles.header}>{section.title}</Typography>
-                  <Table>
-                    <TableBody>
-                      {section.fields && section.fields.map((item, idx) => (
-                          <TableRow key={idx} className={styles.row}>
-                            <TableCell colSpan={2} className={styles.fullWidthCell}>
-                              <Typography variant="subtitle">{item.label}</Typography>
-                              <Typography>{hiddenFields[item.label] ? '•••••••' : item.value}</Typography>
-                              <div className={styles.icons}>
-                                <IconButton onClick={() => navigator.clipboard.writeText(item.value)}>
-                                  <ContentCopyIcon />
-                                </IconButton>
-                                {item.secret && (
-                                    <IconButton onClick={() => toggleVisibility(item.label)}>
-                                      {hiddenFields[item.label] ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                                    </IconButton>
-                                )}
-                                {item.label === 'Website' && (
-                                    <IconButton onClick={() => window.open(item.value, '_blank')}>
-                                      <LinkIcon />
-                                    </IconButton>
-                                )}
-                              </div>
-                            </TableCell>
-                          </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
+    if (data)
+        return (
+            <Box className={styles.root}>
+                {[
+                    { title: 'Item Information', fields: [
+                            { label: 'Name', value: data.name, secret: false },
+                            { label: 'Username', value: data.user_name, secret: false },
+                            { label: 'Password', value: data.password, secret: true }
+                        ]},
+                    { title: 'Website', fields: data.websites?.map(website => ({ label: 'Website', value: website, secret: false })) || []},
+                    { title: 'Custom Fields', fields: data.custom_fields?.map(field => ({ label: field.key, value: field.value, secret: false })) || [] }
+                ].map((section, index) => (
+                    <Paper elevation={3} className={styles.block} key={index}>
+                        <Typography variant="h4" className={styles.header}>{section.title}</Typography>
+                        <Table>
+                            <TableBody>
+                                {section.fields && section.fields.map((item, idx) => (
+                                    <TableRow key={idx} className={styles.row}>
+                                        <TableCell colSpan={2} className={styles.fullWidthCell}>
+                                            <Typography variant="subtext" className={styles.label}>{item.label}</Typography>
+                                            <div className={styles.value}>
+                                                <Typography>{hiddenFields[item.label] ? '•••••••' : item.value}</Typography>
+                                                <div className={styles.icons}>
+                                                    <IconButton onClick={() => navigator.clipboard.writeText(item.value)}>
+                                                        <ContentCopyIcon />
+                                                    </IconButton>
+                                                    {item.secret && (
+                                                        <IconButton onClick={() => toggleVisibility(item.label)}>
+                                                            {hiddenFields[item.label] ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                                        </IconButton>
+                                                    )}
+                                                    {item.label === 'Website' && (
+                                                        <IconButton onClick={() => window.open(item.value, '_blank')}>
+                                                            <LinkIcon />
+                                                        </IconButton>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                ))}
+                <div className={styles.dates}>
+                    <Typography variant="tinytext" className={styles.date}><strong>Updated:</strong> {new Date(data.updated_at).toLocaleString()}</Typography>
+                    <Typography variant="tinytext" className={styles.date}><strong>Created:</strong> {new Date(data.created_at).toLocaleString()}</Typography>
                 </div>
-            ))}
-          </Paper>
-        </Box>
-    );
-  return null;
+            </Box>
+        );
+    return null;
 });
