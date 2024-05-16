fd7fed120ed42b7775d4027d8974952559ae65db
roman_zh1 <roman_zh1@vk.com>
2024-05-17 01:24:02 +0300

    feat: move sidebar layout, add account info buttons

--- frontend/src/pages/root/_components/root-page/index.tsx
+++ frontend/src/pages/root/_components/root-page/index.tsx
@@ -5,26 +5,33 @@ import styles from './index.module.scss';
 import { UserInfoTable } from './components/account-info-table';
 import { UserInfoEdit } from './components/account-info-edit';
 import { IconButton } from '../../../../shared/components/inputs/icon-button';
+import { AccountInfoButtons } from './components/account-info-buttons';
 
 export const RootPage = memo(() => {
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const handleButtonToggle = useCallback(() => {
-    setEditMode(!editMode);
-  }, [editMode]);
-  return (
-    <div className={styles.root}>
-      <div className={styles.content}>
-        <div className={styles.table}>{!editMode ? <UserInfoTable /> : <UserInfoEdit />}</div>
-        <div className={styles.button}>
-          <IconButton onClick={handleButtonToggle}>
-            {!editMode ? (
-              <EditIcon color="secondary" fontSize="small" />
-            ) : (
-              <EditOffOutlinedIcon color="secondary" fontSize="small" />
-            )}
-          </IconButton>
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const handleButtonToggle = useCallback(() => {
+        setEditMode(!editMode);
+    }, [editMode]);
+
+    return (
+        <div className={styles.root}>
+            <div className={styles.content}>
+                <div className={styles.tableWrapper}>
+                    {!editMode ? <UserInfoTable /> : <UserInfoEdit />}
+                </div>
+                <div className={styles.button}>
+                    <IconButton onClick={handleButtonToggle}>
+                        {!editMode ? (
+                            <EditIcon color="secondary" fontSize="small" />
+                        ) : (
+                            <EditOffOutlinedIcon color="secondary" fontSize="small" />
+                        )}
+                    </IconButton>
+                </div>
+            </div>
+            <div className={styles.fixedButtons}>
+                <AccountInfoButtons />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 });
